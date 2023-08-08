a9169dd659b2fe1eefe66c899c9fdb2af8318b95
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-08 22:46:58 +0000

    modified 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     for char in str:
         if ord('A') <= ord(char) <= ord('Z'):
-            print(char, end="")
+            print("{}".format(char), end="")
         elif ord('a') <= ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
             print(char, end="")
