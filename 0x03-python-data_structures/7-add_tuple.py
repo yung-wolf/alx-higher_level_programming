228e58441acc51462e92ba97bb964c5c87b86ea3
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-13 15:46:14 +0000

    fixed tuple = 0 error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,10 +6,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     '''
     if len(tuple_a) == 1:
         tuple_c = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-    elif len(tuple_a) == 0:
+    elif len(tuple_a) == 0 and len(tuple_b) >= 2:
         tuple_c = (0 + tuple_b[0], 0 + tuple_b[1])
+    elif len(tuple_a) == 0 and len(tuple_b) == 1:
+        tuple_c = (0 + 0, 0 + tuple_b[1])
+    elif len(tuple_a) == 0 and len(tuple_b) == 0: 
+        tuple_c = (0 + 0, 0 + 0)
 
-    if len(tuple_b) == 1:
+    elif len(tuple_b) == 1:
         tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
     elif len(tuple_b) == 0:
         tuple_c = (tuple_a[0] + 0, tuple_a[1] + 0)
