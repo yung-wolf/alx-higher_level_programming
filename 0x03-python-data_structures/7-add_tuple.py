08a217f0f4b7fca1826c4af890fdabfb37e482e9
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-13 15:47:35 +0000

    fixed pycodestyle error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,7 +10,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_c = (0 + tuple_b[0], 0 + tuple_b[1])
     elif len(tuple_a) == 0 and len(tuple_b) == 1:
         tuple_c = (0 + 0, 0 + tuple_b[1])
-    elif len(tuple_a) == 0 and len(tuple_b) == 0: 
+    elif len(tuple_a) == 0 and len(tuple_b) == 0:
         tuple_c = (0 + 0, 0 + 0)
 
     elif len(tuple_b) == 1:
