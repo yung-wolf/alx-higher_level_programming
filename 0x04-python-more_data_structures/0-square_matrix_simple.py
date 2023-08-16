ec219ad509d76803cc4ae1663e9b9e2f578fd638
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-16 23:40:01 +0000

    fixed error with different matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,12 +5,16 @@ def square_matrix_simple(matrix=[]):
     lues of all integers in a matrix. Return
     new matrix of same size.
     '''
+    if not matrix:
+        return None
+
     x = len(matrix)
+    y = len(matrix[0])
 
     # copy of matrix
     new_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
     for row in range(x):
-        for column in range(x):
+        for column in range(y):
             new_matrix[row][column] = matrix[row][column] ** 2
 
     return new_matrix
