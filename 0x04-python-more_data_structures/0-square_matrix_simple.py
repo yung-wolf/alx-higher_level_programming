c010a5bc530df87615bcd27b86b49bff0526f519
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-16 22:57:01 +0000

    fixed pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,17 +5,12 @@ def square_matrix_simple(matrix=[]):
     lues of all integers in a matrix. Return
     new matrix of same size.
     '''
-
-    # if empty matrix
-    if not matrix:
-        return None
-
     x = [0, 1, 2]
 
     # copy of matrix
     new_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
     for row in x:
         for column in x:
-           new_matrix[row][column] = matrix[row][column] **2
+            new_matrix[row][column] = matrix[row][column] ** 2
 
-    return new_matrix 
+    return new_matrix
