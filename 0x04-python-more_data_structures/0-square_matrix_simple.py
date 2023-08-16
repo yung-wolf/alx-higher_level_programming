9053e33e8e5f3933b07ba17a5ee8357290993590
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-16 23:04:09 +0000

    handle matrix of different sizes

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,12 +5,12 @@ def square_matrix_simple(matrix=[]):
     lues of all integers in a matrix. Return
     new matrix of same size.
     '''
-    x = [0, 1, 2]
+    x = len(matrix)
 
     # copy of matrix
     new_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-    for row in x:
-        for column in x:
+    for row in range(x):
+        for column in range(x):
             new_matrix[row][column] = matrix[row][column] ** 2
 
     return new_matrix
