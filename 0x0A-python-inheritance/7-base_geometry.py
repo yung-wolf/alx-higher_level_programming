6aeb6b286101f99b0548aedffdaa01a1e3810d04
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-09-11 23:48:49 +0000

    fix 2

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,5 +19,3 @@ class BaseGeometry:
             raise TypeError(f'{name} must be an integer')
         elif value <= 0:
             raise ValueError(f'{name} must be greater than 0')
-        else:
-            pass;
