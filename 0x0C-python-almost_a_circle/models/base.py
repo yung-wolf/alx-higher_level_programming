a126aec73986454743ec5167cf537a55d5e238c7
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-09-19 20:22:18 +0000

    Change file to instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -73,4 +73,34 @@ class Base:
             return dummy_sq
         else:
             raise TypeError('Wrong class entered. Expected either '
-                            'Rectangle of Square class')
+                            'Rectangle or Square class')
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances from a json file"""
+        class_name = cls.__name__
+        if class_name == 'Rectangle':
+            from models.rectangle import Rectangle
+            list_rectangle = []
+            with open(class_name + ".json", "r") as j_file:
+                f_obj = json.load(j_file)  # change to file object
+                for entry in f_obj:  # loop thru each entry in list(f_obj)
+                    r1_json_str = Rectangle.to_json_string(entry)
+                    r1_dic = Rectangle.from_json_string(r1_json_str)
+                    r1 = Rectangle.create(**r1_dic)
+                    list_rectangle.append(r1)  # add obj to list_rect... above
+                return list_rectangle
+        elif class_name == 'Square':
+            from models.square import Square
+            list_square = []
+            with open(class_name + ".json", "r") as j_file:
+                f_obj = json.load(j_file)
+                for entry in f_obj:
+                    sq1_json_str = Square.to_json_string(entry)
+                    sq1_dic = Square.from_json_string(sq1_json_str)
+                    sq1 = Square.create(**sq1_dic)
+                    list_square.append(sq1)
+                return list_square
+        else:
+            raise TypeError('Wrong class entered. Expected either '
+                            'Rectangle or Square class')
