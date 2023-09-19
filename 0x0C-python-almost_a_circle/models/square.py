9b1c3abda273101a35b90989c57079e1781373f1
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-09-19 10:49:58 +0000

    removed size attribute from init

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,7 +22,6 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.__size = size
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
