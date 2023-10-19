9d2ecc15937c105cc5b37b7e1117e05d031507de
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-10-19 04:47:04 +0000

    time gap fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
--- Results must be sorted in ascending order by cities.id
+-- results must be sorted in ascending order by cities.id
 USE hbtn_0d_usa;
 SELECT cities.id, cities.name
 FROM cities
