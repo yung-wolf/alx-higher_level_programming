7c94586dedf2b61381f9f38048ddab00a92e773a
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-10-19 03:07:30 +0000

    make 'Dexter' search key

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- hbtn_0d_tvshows database to lists all genres of the show Dexter
 -- Each record should display: tv_genres.name
 SELECT tv_genres.name
-FROM tv_genres
-  INNER JOIN tv_show_genres
-WHERE tv_show_genres.show_id = 8 AND tv_show_genres.genre_id = tv_genres.id
+FROM tv_shows
+  INNER JOIN tv_show_genres ON tv_shows.title = 'Dexter' AND tv_shows.id = tv_show_genres.show_id
+  INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_genres.name;
