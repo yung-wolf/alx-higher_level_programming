bda466cf03cebef1d027cd2ece07094569d92582
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-10-19 03:16:02 +0000

    make state name 'California' search key

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,8 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 -- Results must be sorted in ascending order by cities.id
 USE hbtn_0d_usa;
-SELECT id, name FROM cities
-  WHERE state_id = 1
-  ORDER BY id;
+SELECT cities.id, cities.name
+FROM cities
+  JOIN states
+WHERE states.name = 'California' AND cities.state_id = states.id
+ORDER BY cities.id;
