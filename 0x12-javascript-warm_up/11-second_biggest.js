108719c99a914429374884b9d41245c44fa961a3
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-11-14 01:23:02 +0000

    set first run of

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
-  let secondBigNum = 0;
+  let secondBigNum = -1000;
   let biggestNum = parseInt(process.argv[2]);
 
   process.argv.forEach((val, index) => {
