93865ae420d88ea02cd6dd56071ed1e3e1307a9d
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-11-15 13:17:45 +0000

    Set biggestNum init

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   let secondBigNum = -1000;
-  let biggestNum = parseInt(process.argv[2]);
+  let biggestNum = -1000;
 
   process.argv.forEach((val, index) => {
     if (parseInt(val) > biggestNum) {
