9a8a12a49c13d059e58bb2c43f589ed348948d44
yung-wolf <edmundbansah@gmail.com>
2023-11-19 07:54:32 -0800

    prints the first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Module: 8-model_state_fetch_first
+
+Fetch the first state obj from db hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine, Table, MetaData
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    Base = declarative_base()
+
+    db_type = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(
+            db_type.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True
+            )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).filter_by(id=1).first()
+    print(f"{first_state.id}: {first_state.name}")
