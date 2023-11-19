40fab18385a5e62f211f9ea9ecc5b8317e8a311b
yung-wolf <edmundbansah@gmail.com>
2023-11-19 11:24:53 -0800

    deletes instances with letter [a]

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+Module: 13-model_state_delete_a
+
+deletes all State objects that contain the letter a from the database
+hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine, Table, MetaData
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    Base = declarative_base()
+
+    db_type = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(
+            db_type.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True
+            )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
+
+    session.commit()
