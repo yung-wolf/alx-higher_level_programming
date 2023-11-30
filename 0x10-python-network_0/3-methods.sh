024967ae7212679a153038d2c9178622af0148bd
ELI (yung-wolf) <edmundbansah@gmail.com>
2023-11-30 15:07:48 -0800

    Fix requirement error. Max 3 lines per file

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays all HTTP methods the server
-# will accept.
-
-url=$1
-curl -sI "$url" | grep 'Allow:' | cut -f2- -d' '
+# Bash script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI "$1" | grep 'Allow:' | cut -f2- -d' '
